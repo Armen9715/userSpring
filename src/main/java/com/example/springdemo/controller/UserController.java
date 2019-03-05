d0ade28c745498713a1b052245fd49795492bfa6
armen27gh <48012934+armen27gh@users.noreply.github.com>
2019-03-06 00:55:14 +0400

    userSpring,

--- src/main/java/com/example/springdemo/controller/UserController.java
+++ src/main/java/com/example/springdemo/controller/UserController.java
@@ -1,21 +1,24 @@
 package com.example.springdemo.controller;
 
 import com.example.springdemo.model.User;
+import com.example.springdemo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping ("/user")
+@RequestMapping("/user")
 public class UserController {
+    @Autowired
+    UserRepository userRepository;
 
     @PostMapping("/add")
-    public String add(@RequestParam("name") String name, @RequestParam("surname") String surname){
-       User user = User.builder()
-               .name(name)
-               .surname(surname)
-               .build();
+    public String add(@RequestParam("name") String name, @RequestParam("surname") String surname) {
+        User user = User.builder().name(name).surname(surname).build();
+        userRepository.save(user);
+        System.out.println(user);
         return "redirect:/home";
     }
 }
